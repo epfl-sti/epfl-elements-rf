24a73cbb140e3765262a898c82d1df90903df2e4
Andrii Babarytskyi <andrii.babarytskyi@epfl.ch>
2025-09-16 14:18:42 +0200

    Add fallback directly to element

--- src/components/navigations/avatar.tsx
+++ src/components/navigations/avatar.tsx
@@ -73,7 +73,15 @@ export function Avatar({ profileUrl, logoutUrl, user, customAvatarSectionHTML, m
         <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded={showUserMenu ? "true" : "false"}
           onClick={() => setShowUserMenu(!showUserMenu)}
         >
-          <img className="user-avatar rounded-circle" src={user.photoUrl} alt="" />
+          <img
+            className="user-avatar rounded-circle"
+            src={user.photoUrl}
+            alt=""
+            onError={(e) => {
+              e.currentTarget.src =
+                "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTYgMjU2Ij48cGF0aCBkPSJNMTAyLjQzIDEyNC41M1MxMDUuNSAxNTIgMTI4IDE1MnMyNS41Ny0yNy40NiAyNS41Ny0yNy40NiA0Ljg1IDEgNy4xOS05LjI4YzEuNS02LjctLjUtOC40OS0xLjc2LTguNDloLTEuMkMxNjMuNjggNzQuMjggMTQwIDY0IDEyOCA2NHMtMzUuNjggMTAuMjgtMjkuOCA0Mi43Nkg5N2MtMS4yNiAwLTMuMjYgMS43OS0xLjc2IDguNDkgMi4zNCAxMC4zMyA3LjE5IDkuMjggNy4xOSA5LjI4ek0xNzAuMzYgMTY0Yy0yMC4yNi0zLjg5LTI0LjM0LTgtMjUuMS0xMS42NGEyOS4xNSAyOS4xNSAwIDAgMS0zNC41MiAwYy0uNzQgMy42NC00Ljg0IDcuNzItMjUuMSAxMS42NC0yMC44NSA0LTIwLjU3IDIxLjE4LTIwLjU3IDI0aDEyNS44NmMwLTIuODUuMjgtMjAtMjAuNTctMjR6Ii8+PC9zdmc+Cg==";
+            }}
+          />
           <p className="user-name">{user.firstName} {user.lastName}</p>
         </button>
         <ul className={`dropdown-menu ${showUserMenu ? "show" : ""}`} aria-labelledby="dropdownMenuButton">
