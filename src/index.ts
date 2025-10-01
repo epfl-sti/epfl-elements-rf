238f8b2923cbeba56abfe4c78425a0a4f80310b9
Juan Convers <me@jco.im>
2025-10-01 10:45:50 +0200

    Exporting missing modal component.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,55 @@
-// Import the EPFL elements at the top. 
-import '@epfl-sti/epfl-elements-styles/dist/css/combined.css'
+// Import the EPFL elements at the top.
+import "@epfl-sti/epfl-elements-styles/dist/css/combined.css";
 
-export { Alert } from './components/Alert'
-export { Carousel } from './components/Carousel'
-export { Drawer } from './components/Drawer'
-export { Breadcrumbs } from './components/Breadcrumbs'
-export { Collapse } from './components/Collapse'
-export { Content } from './components/Content'
-export { ExternalLink } from './components/Links/'
-export { Error } from './components/Error'
-export { Figures } from './components/Figures'
-export { Fullwidthteaser } from './components/Fullwidthteaser'
-export { Hero } from './components/Hero'
-export { Loader } from './components/Loader'
-export { Logo } from './components/Logo'
-export { Metabox } from './components/Metabox'
-export { Table } from './components/Table'
-export { Tag } from './components/Tag'
-export { Tagset } from './components/Tagset'
-export { Video } from './components/Video'
+export { Alert } from "./components/Alert";
+export { Carousel } from "./components/Carousel";
+export { Drawer } from "./components/Drawer";
+export { Breadcrumbs } from "./components/Breadcrumbs";
+export { Collapse } from "./components/Collapse";
+export { Content } from "./components/Content";
+export { ExternalLink } from "./components/Links/";
+export { Error } from "./components/Error";
+export { Figures } from "./components/Figures";
+export { Fullwidthteaser } from "./components/Fullwidthteaser";
+export { Hero } from "./components/Hero";
+export { Loader } from "./components/Loader";
+export { Logo } from "./components/Logo";
+export { Modal } from "./components/Modal";
+export { Metabox } from "./components/Metabox";
+export { Table } from "./components/Table";
+export { Tag } from "./components/Tag";
+export { Tagset } from "./components/Tagset";
+export { Video } from "./components/Video";
 
 // Cards
-export { Card } from './components/Card'
+export { Card } from "./components/Card";
 
 // Forms
-export { Button } from './components/Forms/Button'
-export { Checkbox } from './components/Forms/Checkbox'
-export { CheckboxGroup } from './components/Forms/CheckboxGroup'
-export { CsfrToken } from './components/Forms/Csfrtoken'
-export { Dropdown } from './components/Forms/Dropdown'
-export { Input } from './components/Forms/Input'
-export { FilterBox } from './components/Filterbox'
-export { Range } from './components/Forms/Range'
-export { Switch } from './components/Forms/Switch'
+export { Button } from "./components/Forms/Button";
+export { Checkbox } from "./components/Forms/Checkbox";
+export { CheckboxGroup } from "./components/Forms/CheckboxGroup";
+export { CsfrToken } from "./components/Forms/Csfrtoken";
+export { Dropdown } from "./components/Forms/Dropdown";
+export { Input } from "./components/Forms/Input";
+export { FilterBox } from "./components/Filterbox";
+export { Range } from "./components/Forms/Range";
+export { Switch } from "./components/Forms/Switch";
 
 // Navigations
-export { Asidemenu } from './components/navigations/asidemenu'
-export { Avatar } from './components/navigations/avatar'
-export { Language } from './components/navigations/language'
-export { MainMenu } from './components/navigations/mainMenu'
-export { Topmenu } from './components/navigations/topmenu'
+export { Asidemenu } from "./components/navigations/asidemenu";
+export { Avatar } from "./components/navigations/avatar";
+export { Language } from "./components/navigations/language";
+export { MainMenu } from "./components/navigations/mainMenu";
+export { Topmenu } from "./components/navigations/topmenu";
 
 // Sections
-export { Footer } from './components/Footer/Footer'
-export { FooterLight } from './components/Footer/FooterLight'
-export { Header } from './components/Header'
-export { Tabset } from './components/Tabset'
+export { Footer } from "./components/Footer/Footer";
+export { FooterLight } from "./components/Footer/FooterLight";
+export { Header } from "./components/Header";
+export { Tabset } from "./components/Tabset";
 
 // Tabset Helper
-export { tabUpdateContents } from './components/Tabset/helpers'
+export { tabUpdateContents } from "./components/Tabset/helpers";
 
 // Container
-export { Base } from './components/Base'
+export { Base } from "./components/Base";
